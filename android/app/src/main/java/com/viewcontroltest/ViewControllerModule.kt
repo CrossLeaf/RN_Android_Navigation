80c7ba70477fda20b3062299c36dceaf626f9379
Eton <pk2p007@gmail.com>
2020-03-12 15:47:02 +0800

    🆕navigation 使用
    🎉 	初次 commit
    🆕 	引入新功能
    🔖 	發行/版本標籤
    🐛 	修復 bug
    🚑 	重要補丁
    🌐 	國際化与本地化
    💄 	更新 UI 和樣式文件
    🎬 	更新演示/示例
    🚨 	移除 linter 警告
    🔧 	修改配置文件
    ➕ 	增加一個依賴
    ➖ 	減少一個依賴
    ⬆️ 	升級依賴
    ⬇️ 	降級依賴
    🐎 	提升性能
    📈 	添加分析或跟踪程式碼
    🚀 	部署功能
    ✅ 	增加測試
    📝 	撰寫文檔
    🔨 	重大重構
    🎨 	改進程式碼結構/程式碼格式
    🔥 	移除程式碼或文件
    ✏️ 	修復 typo
    🚧 	工作進行中
    🗑 	廢棄或刪除
    ♿️ 	可訪問性
    👷 	添加 CI 構建系统
    💚 	修復 CI 構建問題
    🔒 	修復安全問題
    🐳 	Docker 相關工作
    🍎 	修復 macOS 下的問題
    🐧 	修復 Linux 下的問題
    🏁 	修復 Windows 下的問題

--- android/app/src/main/java/com/viewcontroltest/ViewControllerModule.kt
+++ android/app/src/main/java/com/viewcontroltest/ViewControllerModule.kt
@@ -1,5 +1,6 @@
 package com.viewcontroltest
 
+import android.content.Intent
 import android.util.Log
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -14,5 +15,7 @@ class ViewControllerModule(reactContext: ReactApplicationContext) : ReactContext
     @ReactMethod
     fun showActivity() {
         Log.d("ViewController", "show activity.")
+        val intent = Intent(currentActivity, MainActivity2::class.java)
+        currentActivity?.startActivity(intent)
     }
 }
