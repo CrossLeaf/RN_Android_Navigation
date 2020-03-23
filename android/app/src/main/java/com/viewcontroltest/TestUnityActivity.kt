f8a50695a6ebcd835ef3ae54a7a76d3ebf37a2ec
Eton <pk2p007@gmail.com>
2020-03-23 10:21:48 +0800

    🆕導入 Unity 包
    🎉 	初次 commit
    🆕 	引入新功能
    🔖 	發行/版本標籤
    🐛 	修復 bug
    🚑 	重要補丁
    🌐 	國際化与本地化
    💄 	更新 UI 和樣式文件
    🎬 	更新演示/示例
    🚨 	移除 linter 警告
    🔧 	修改配置文件
    ➕ 	增加一個依賴
    ➖ 	減少一個依賴
    ⬆️ 	升級依賴
    ⬇️ 	降級依賴
    🐎 	提升性能
    📈 	添加分析或跟踪程式碼
    🚀 	部署功能
    ✅ 	增加測試
    📝 	撰寫文檔
    🔨 	重大重構
    🎨 	改進程式碼結構/程式碼格式
    🔥 	移除程式碼或文件
    ✏️ 	修復 typo
    🚧 	工作進行中
    🗑 	廢棄或刪除
    ♿️ 	可訪問性
    👷 	添加 CI 構建系统
    💚 	修復 CI 構建問題
    🔒 	修復安全問題
    🐳 	Docker 相關工作
    🍎 	修復 macOS 下的問題
    🐧 	修復 Linux 下的問題
    🏁 	修復 Windows 下的問題

--- android/app/src/main/java/com/viewcontroltest/TestUnityActivity.kt
+++ android/app/src/main/java/com/viewcontroltest/TestUnityActivity.kt
@@ -1,12 +1,21 @@
 package com.viewcontroltest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_BACK
 import com.unity3d.player.UnityPlayerActivity
 
 class TestUnityActivity : UnityPlayerActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_test_unity)
+    }
+
+    override fun onKeyDown(p0: Int, p1: KeyEvent?): Boolean {
+        if (p0 == KEYCODE_BACK) {
+            finish()
+//            return true
+        }
+        return super.onKeyDown(p0, p1)
     }
 }
